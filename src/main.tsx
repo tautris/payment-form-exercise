f0972a4bcf4626b7ef172a088229629e0a20a22b
tautrimas_gumbys <tautrimas.gumbys@gmail.com>
2026-05-30 13:38:08 +0300

    Add material UI with fonts, basic theme and css baseline for sensible defaults

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
+import { CssBaseline, createTheme, ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import "./index.css";
 import App from "./App.tsx";
 
@@ -9,8 +16,17 @@ if (!rootElement) {
 	throw new Error("Root element not found");
 }
 
+const theme = createTheme({
+	palette: {
+		mode: "light",
+	},
+});
+
 createRoot(rootElement).render(
 	<StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>{" "}
 	</StrictMode>,
 );
