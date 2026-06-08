223b454058e100ac23803b6ddd87161bcfea8fc7
tautrimas_gumbys <tautrimas.gumbys@gmail.com>
2026-06-08 18:43:38 +0300

    Add vitest, setup for node env tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
 	base: command === "build" ? "/payment-form-exercise/" : "/",
 	plugins: [react()],
+	test: {
+		environment: "node",
+	},
 }));
